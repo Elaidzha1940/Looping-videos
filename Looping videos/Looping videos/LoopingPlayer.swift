42fd2901a4ae2107d51dd12c9110054990e37a56
Elaidzha1940 <eli080293@gmail.com>
2023-04-04 21:02:21 +0700

    Looping videos + added an Image + added Video + added a new File

---
+++ Looping videos/Looping videos/LoopingPlayer.swift
@@ -0,0 +1,20 @@
+//
+//  LoopingPlayer.swift
+//  Looping videos
+//
+//  Created by Elaidzha Shchukin on 04.04.2023.
+//
+
+import SwiftUI
+
+struct LoopingPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoopingPlayer_Previews: PreviewProvider {
+    static var previews: some View {
+        LoopingPlayer()
+    }
+}
