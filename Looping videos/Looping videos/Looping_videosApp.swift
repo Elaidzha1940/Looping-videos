a3ce7bde423334afc551a094ff5aceb5c5a0f3d3
Elaidzha1940 <eli080293@gmail.com>
2023-04-04 20:42:48 +0700

    Looping videos

---
+++ Looping videos/Looping videos/Looping_videosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Looping_videosApp.swift
+//  Looping videos
+//
+//  Created by Elaidzha Shchukin on 04.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Looping_videosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
