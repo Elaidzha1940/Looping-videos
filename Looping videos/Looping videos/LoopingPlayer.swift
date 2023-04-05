d1ab1655da1a286a94abcda2c383a98e9a9b6f68
Elaidzha1940 <eli080293@gmail.com>
2023-04-05 13:05:16 +0700

    Looping videos + added an Image + added Video + added a new File + AVFoundation

--- Looping videos/Looping videos/LoopingPlayer.swift
+++ Looping videos/Looping videos/LoopingPlayer.swift
@@ -23,17 +23,25 @@ class PlayerUIView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        //Load Video
+        let fileUrl = Bundle.main.url(forResource: "VidoeWithBlock", withExtension: "move")
+        let playerItem = AVPlayerItem(url: fileUrl)
+        
+        //Setup Player
+        let player = AVPlayerLayer(playerItem: playerItem)
+        playerLayer.player = player
+        playerLayer.vidoeGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer)
+        
+        //Play
+        player.play()
     }
     
-    //Load Video
-    let fileUrl = Bundle.main.url(forResource: "VidoeWithBlock", withExtension: "move")
-    let playerItem = AVPlayerItem(url: fileUrl)
-    
-    //Setup Player
-    let player = AVPlayerLayer(playerItem: playerItem)
-    
-    //Play
-    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
