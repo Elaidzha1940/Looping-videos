92296268eab71980d2f1742046989f90985d0cdd
Elaidzha1940 <eli080293@gmail.com>
2023-04-05 12:16:28 +0700

    Looping videos + added an Image + added Video + added a new File + ...

--- Looping videos/Looping videos/LoopingPlayer.swift
+++ Looping videos/Looping videos/LoopingPlayer.swift
@@ -18,7 +18,10 @@ class PlayerUIView: UIView {
         super.init(frame: frame)
     }
     
-    required 
+    required init?(coder: NSCoder) {
+        
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 struct LoopingPlayer_Previews: PreviewProvider {
