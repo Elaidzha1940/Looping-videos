1079066992321c268f8b83b49905cb50ee38d0c0
Elaidzha1940 <eli080293@gmail.com>
2023-04-05 12:50:27 +0700

    Looping videos + added an Image + added Video + added a new File + ...

--- Looping videos/Looping videos/LoopingPlayer.swift
+++ Looping videos/Looping videos/LoopingPlayer.swift
@@ -11,6 +11,10 @@ struct LoopingPlayer: UIViewRepresentable {
     func makeUIView(context: Context) -> some UIView {
         return PlayerUIView(frame: .zero)
     }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        //Noting is here
+    }
 }
 
 class PlayerUIView: UIView {
@@ -18,8 +22,16 @@ class PlayerUIView: UIView {
         super.init(frame: frame)
     }
     
+    //Load Video
+    let fileUrl = Bundle.main.url(forResource: "VidoeWithBlock", withExtension: "move")
+    let Pla
+    
+    //Setup Player
+    
+    //Play
+    
+    
     required init?(coder: NSCoder) {
-        
         fatalError("init(coder:) has not been implemented")
     }
 }
