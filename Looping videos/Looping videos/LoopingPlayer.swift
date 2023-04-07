9aff4d8745ceac45fbff74d028ca7bc0925d85da
Elaidzha1940 <eli080293@gmail.com>
2023-04-07 15:49:22 +0700

    LoopVideos + Fixing Errors

--- Looping videos/Looping videos/LoopingPlayer.swift
+++ Looping videos/Looping videos/LoopingPlayer.swift
@@ -66,3 +66,42 @@ struct LoopingPlayer_Previews: PreviewProvider {
         LoopingPlayer()
     }
 }
+
+class QueuePlayerUIView: UIView {
+    private var playerLayer = AVPlayerLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //Load Video
+        let fileUrl = Bundle.main.url(forResource: "VidoeWithBlock", withExtension: "move")
+        let playerItem = AVPlayerItem(url: fileUrl)
+        
+        //Setup Player
+        let player = AVPlayer(playerItem: playerItem)
+        playerLayer.player = player
+        playerLayer.vidoeGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer)
+        
+        //Loop
+        player.actionAtItemEnd = .none
+        NotificationCenter.default.addObserver(self, selector: #selector(rewindVideo(notification: )), name: .AVPlayerItemDidPlayToEndTime, object: player.currentTime())
+        
+        //Play
+        player.play()
+    }
+    
+    @objc
+    func rewindVideo(notification: Notification) {
+        playerLayer.player?.seek(to: .zero)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
