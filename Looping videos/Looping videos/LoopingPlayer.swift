552c9a08f00225db0bbc7904a75dced2c8c26838
Elaidzha1940 <eli080293@gmail.com>
2023-04-07 15:55:28 +0700

    LoopVideos + Fixing Errors

--- Looping videos/Looping videos/LoopingPlayer.swift
+++ Looping videos/Looping videos/LoopingPlayer.swift
@@ -14,11 +14,45 @@ import AVFoundation
 
 struct LoopingPlayer: UIViewRepresentable {
     func makeUIView(context: Context) -> some UIView {
-        return PlayerUIView(frame: .zero)
+        return QueuePlayerUIView(frame: .zero)
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
         //Noting is here
+    }
+}
+
+class QueuePlayerUIView: UIView {
+    private var playerLayer = AVPlayerLayer()
+    private var playerLooper: AVPlayerLooper?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //Load Video
+        let fileUrl = Bundle.main.url(forResource: "VidoeWithBlock", withExtension: "move")
+        let playerItem = AVPlayerItem(url: fileUrl)
+        
+        //Setup Player
+        let player = AVQueuePlayer(playerItem: playerItem)
+        playerLayer.player = player
+        playerLayer.vidoeGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer)
+        
+        //Loop
+        playerLooper = AVPlayerLooper(player: player, templateItem: playerItem)
+    
+        //Play
+        player.play()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
@@ -67,41 +101,3 @@ struct LoopingPlayer_Previews: PreviewProvider {
     }
 }
 
-class QueuePlayerUIView: UIView {
-    private var playerLayer = AVPlayerLayer()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        //Load Video
-        let fileUrl = Bundle.main.url(forResource: "VidoeWithBlock", withExtension: "move")
-        let playerItem = AVPlayerItem(url: fileUrl)
-        
-        //Setup Player
-        let player = AVPlayer(playerItem: playerItem)
-        playerLayer.player = player
-        playerLayer.vidoeGravity = .resizeAspectFill
-        layer.addSublayer(playerLayer)
-        
-        //Loop
-        player.actionAtItemEnd = .none
-        NotificationCenter.default.addObserver(self, selector: #selector(rewindVideo(notification: )), name: .AVPlayerItemDidPlayToEndTime, object: player.currentTime())
-        
-        //Play
-        player.play()
-    }
-    
-    @objc
-    func rewindVideo(notification: Notification) {
-        playerLayer.player?.seek(to: .zero)
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        playerLayer.frame = bounds
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
